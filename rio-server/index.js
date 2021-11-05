51d98501bae05fee061cf9f53c754fb21c87bc2c
blauzaaan <28716906+blauzaaan@users.noreply.github.com>
2021-11-05 10:58:58 +0100

    added default text

--- rio-server/index.js
+++ rio-server/index.js
@@ -20,6 +20,7 @@ const wsClient = require('./clients/websocket-client');
 const twitterClient = require('./clients/twitter-client');
 
 const getGIFData = require('./inputs/gif-url-input');
+const getTextData = require('./inputs/text-input');
 const getVideoData = require('./inputs/video-url-input');
 
 app.use(function(req, res, next) {
@@ -40,13 +41,18 @@ wsClient.init(app);
 // Startup content
 const startup = () => {
     //const getImageData = require('./inputs/image-url-input');
-    getGIFData('https://media.giphy.com/media/xUA7b3zUuoScFWe3bW/giphy.gif'); // rio
+
+    // giphy links are broken, just for reference
+    // getGIFData('https://media.giphy.com/media/xUA7b3zUuoScFWe3bW/giphy.gif'); // rio
     // getGIFData('https://media.giphy.com/media/l3vRjnRH14rBOlUqY/source.gif'); // 50fps 50x30 RGB rect
     // getGIFData('http://i.giphy.com/3oz8xEMwRxFQV21ntC.gif'); // 5s 6x5 stripey GIF
     // getGIFData('http://i.giphy.com/l0MYE0GTUJFY5PWcU.gif'); // 30x1 5s 2 frame GIF
     // getGIFData('http://i.giphy.com/3oz8xwDZ6N4vt2D6so.gif'); // 5s 12x10 stripey GIF
     //
     // getVideoData('https://www.youtube.com/watch?v=wS8ZC271eMQ');
+
+    //getGIFData('https://64.media.tumblr.com/fc031ba2e1d62b29056e925dcaeb3e2b/tumblr_ngqeny4hUE1rpj04no1_500.gif');
+    getTextData('test');
 }
 
 // todo: This should be improved
